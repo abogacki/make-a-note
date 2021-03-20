82cb0155f41508cebc4d404bfbdfdbe99bf921d7
abogacki <anbogacki@gmail.com>
2021-03-20 01:02:24 +0100

    Add docker config for server

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,6 @@ app.get("/", function (_req, res) {
   res.send("Hello World!");
 });
 
-app.listen(3000, function () {
-  console.log("Example app listening on port 3000!");
+app.listen(3002, function () {
+  console.log("Example app listening on port 300 asds as0!");
 });
